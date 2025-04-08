093e359fd374c567f2df8d3951ea8d906c529de4
Artem322-v2 <zakiev2100@mail.ru>
2025-04-08 18:40:39 +0400

    Примитивные типы данных

---
+++ script.js
@@ -0,0 +1,27 @@
+// let str = 'JavaScript is fun'
+
+// console.log(str.includes('fun'))
+
+
+// let user;
+
+// if ((user === null) || (user === undefined)) {
+//     console.log('Сделал')
+// }
+
+
+// let firstName = 'John'
+// let lastName = 'Doe'
+// let occupation = 'software developer'
+// const result = `Hello, my name is ${firstName} ${lastName}. I am a ${occupation}`
+// console.log(result)
+
+
+// let user;
+// let user2 = null;
+
+// console.log(user === user2)
+// console.log(user == user2) // такой результат получился потому что при === сравнивается еще и тип данных, а null это объект
+
+
+console.log(1 + '1') // он перевел 1 в тип строку и объединил одну строку с другой
